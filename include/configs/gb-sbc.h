e6d118d60c7d7192c8c57b01d3194311dca888a7
Eran <erang@hailo.ai>
2024-02-11 09:18:40 +0200

    gb sbc devtree

---
+++ include/configs/gb-sbc.h
@@ -0,0 +1,14 @@
+#ifndef __GB_SBC_H 
+	#define __GB_SBC_H
+	#include "hailo15_common.h"
+	#ifdef CONFIG_HAILO15_DDR_ENABLE_ECC	
+		/* Bank 0 size using ECC */
+		#define PHYS_SDRAM_1_SIZE (0x70000000)
+		/* Bank 1 address/size using ECC */
+		#define PHYS_SDRAM_2 (0x100000000)
+		#define PHYS_SDRAM_2_SIZE (0x70000000)
+	#else
+		/* Bank 0 size not using ECC */
+		#define PHYS_SDRAM_1_SIZE (0x100000000)
+	#endif
+#endif
